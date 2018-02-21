3ae05e49309354227428ba3c02e13596f36e09d5
migrow <michael.growette@gmail.com>
2018-02-21 15:03:29 -0500

    restricting rows returned

--- student-exercises/growette-select.sql
+++ student-exercises/growette-select.sql
@@ -2,3 +2,11 @@ use rockStarDay2;
 show columns from band;
 
 select ID, name, genre from band;
+
+select * from individual where deceasedDate is not null;
+
+select ID, lastName from individual where deceasedDate is not null;
+
+select ID, lastName, birthDate from individual where year(birthDate)>1940;
+
+select * from individual where ID in (1,3,5,7,19);
