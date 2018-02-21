c14b19359dfdbfcb794d97eff4a7f3708d089f86
migrow <michael.growette@gmail.com>
2018-02-21 15:11:03 -0500

    translating requirements to sql statements

--- student-exercises/growette-select.sql
+++ student-exercises/growette-select.sql
@@ -1,12 +1,4 @@
 use rockStarDay2;
-show columns from band;
 
-select ID, name, genre from band;
-
-select * from individual where deceasedDate is not null;
-
-select ID, lastName from individual where deceasedDate is not null;
-
-select ID, lastName, birthDate from individual where year(birthDate)>1940;
-
-select * from individual where ID in (1,3,5,7,19);
+select * from band where genre = 'Alternative' and isTogether = 0;
+select ID, name from band where ID=4;
